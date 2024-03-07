001d52550baa1c7eacdb086622a3a458b52fe69a
valer1435 <valp998@gmail.com>
2024-03-07 14:36:14 +0300

    fix

--- utils.py
+++ utils.py
@@ -52,7 +52,7 @@ def get_branch_files(pr, branch, headers):
     parts = original_url.split("/")
     owner, repo = parts[-4], parts[-3]
     url = f"https://api.github.com/repos/{owner}/{repo}/git/trees/{branch}?recursive=1"
-    print(branch)
+    print(owner, repo, branch)
     response = requests.get(url, headers=headers)
     tree = response.json().get('tree', [])
     files = {}
